160caafcba9a9abaf9e86511609f3002c0c1bae5
Iago <iagobrazmendes@gmail.com>
2020-11-12 10:50:56 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>STEM Guy</title>
+      </Head>
+
+      <h1>Hi there!</h1>
+    </div>
+  )
+}
