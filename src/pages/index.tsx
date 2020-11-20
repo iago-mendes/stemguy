1e10a67043230b07179d8f4500aa04b85af5f98b
Iago <iagobrazmendes@gmail.com>
2020-11-20 09:55:55 -0300

    handle search in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,47 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import {useEffect, useState} from 'react'
+import useSWR from 'swr'
 
-const Home: React.FC = () =>
+import Loading from '../components/Loading'
+import api from '../services/api'
+
+interface Post
 {
+	id: string
+	url_id: string
+	title: string
+	description: string
+	image: string
+	flags: Array<
+	{
+		name: string
+		color: string
+	}>
+}
+
+interface HomeProps
+{
+	staticPosts: Post[]
+}
+
+const Home: React.FC<HomeProps> = ({staticPosts}) =>
+{
+	const [search, setSearch] = useState('')
+	const [posts, setPosts] = useState<Post[]>([])
+	const {data, error} = useSWR(`/api/search?q=${search}`)
+
+	useEffect(() =>
+	{
+		if (search === '') setPosts(staticPosts)
+		else if (data) setPosts(data.posts)
+
+		if (data) console.log('[data]', data.posts)
+		console.log('[search]', search)
+	}, [data, search])
+
+	if (error) return <h1>failed to load data</h1>
+
 	return (
 		<div className="page">
 			<Head>
@@ -9,9 +49,32 @@ const Home: React.FC = () =>
 				<meta name='thumbnail' content='/logo.svg' />
 			</Head>
 
-			<h1>Home</h1>
+			<header>
+				<input type="text" value={search} onChange={e => setSearch(e.target.value)} />
+			</header>
+
+			<main>
+				{
+					!data
+					? <Loading />
+					: posts.length === 0
+						? <h1>No results found!</h1>
+						: posts.map(post => post.title)
+				}
+			</main>
 		</div>
 	)
+}
+
+export const getStaticProps: GetStaticProps = async ctx =>
+{
+	let posts = []
+	await api.get('posts').then(res => posts = res.data)
+
+	return {
+		props: {staticPosts: posts},
+		revalidate: 10
+	}
 }
 
 export default Home
