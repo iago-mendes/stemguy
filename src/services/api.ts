156216d0407ec242def9cede249492f15106ebb5
Iago <iagobrazmendes@gmail.com>
2020-11-25 11:41:20 -0300

    fix connection to api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,11 @@ import axios from 'axios'
 
 const api = axios.create(
 {
-	baseURL: process.env.API_URL
+	baseURL: process.env.API_URL,
+	headers:
+	{
+		key: process.env.API_KEY
+	}
 })
 
 export default api
