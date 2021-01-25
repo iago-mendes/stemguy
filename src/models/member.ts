46035b72feb110514bc6f40d9ce7fc90e3f0d12f
Iago <iagobrazmendes@gmail.com>
2021-01-25 12:20:42 -0300

    get members info

---
+++ src/models/member.ts
@@ -0,0 +1,22 @@
+interface Member
+{
+  id: string
+  name: string
+  image: string
+  role: string
+  admin: boolean
+  bio: string
+  favTopics: string[]
+}
+
+export interface MemberListed
+{
+  id: string
+  name: string
+  image: string
+  role: string
+  bio: string
+  favTopics: string[]
+}
+
+export default Member
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,10 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { GetStaticProps } from 'next'
 
 import Container from '../../styles/pages/about/index'
 import logo from '../../assets/logo.svg'
+import { MemberListed } from '../../models/member'
+import api from '../../services/api'
+import Loading from '../../components/Loading'
 
-const About: React.FC = () =>
+interface AboutProps
+{
+	members: MemberListed[]
+}
+
+const About: React.FC<AboutProps> = ({members}) =>
 {
 	return (
 		<Container className='page' >
@@ -23,8 +32,43 @@ const About: React.FC = () =>
 					</div>
 				</div>
 			</main>
+
+			<div className='team'>
+				<label>Meet our team</label>
+				<ul>
+					{members.map(member => (
+						<div className='member' key={member.id}>
+							<div className='img'>
+								<Image src={member.image} width={1000} height={1000} layout='responsive'/>
+							</div>
+							<div className='info'>
+								<h1>{member.name}</h1>
+								<h2>{member.role}</h2>
+								<p>{member.bio}</p>
+								<div className='topics'>
+									{member.favTopics.map((topic, index) => (
+										<span key={index} >
+											{topic}
+										</span>
+									))}
+								</div>
+							</div>
+						</div>
+					))}
+				</ul>
+			</div>
 		</Container>
 	)
+}
+
+export const getStaticProps: GetStaticProps = async ctx =>
+{
+	const {data} = await api.get('members')
+
+	return {
+		props: {members: data},
+		revalidate: 60
+	}
 }
 
 export default About
