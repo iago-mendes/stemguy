1ceca34d9a321b2f0a12b5d95d2d5757e0c13b33
Iago <iagobrazmendes@gmail.com>
2021-02-11 11:08:05 -0300

    improve footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
+import Image from 'next/image'
 import Link from 'next/link'
-import { FiInstagram, FiTwitter } from 'react-icons/fi'
+import {FiInstagram, FiTwitter} from 'react-icons/fi'
 
 import Container from '../styles/components/Footer'
 
@@ -7,31 +8,42 @@ const Footer: React.FC = () =>
 {
 	return (
 		<Container>
-			<div className='group'>
-				<h1>Site links</h1>
-				<Link href='/' >
-					<a className='link' >Home</a>
-				</Link>
-				<Link href='/about' >
-					<a className='link' >About</a>
-				</Link>
-				<Link href='/policies/privacy' >
-					<a className='link' >Privacy policy</a>
-				</Link>
-				<Link href='/policies/terms-of-use' >
-					<a className='link' >Terms of use</a>
-				</Link>
+			<div className='logos'>
+				<div className='icon'>
+					<Image src='/logo-darked.svg' alt='STEM Guy icon' width={1000} height={1000} layout='responsive' />
+				</div>
+				<div className='name'>
+					<Image src='/logo-name.svg' alt='STEM Guy' width={1000} height={250} layout='responsive' />
+				</div>
 			</div>
-			<div className='group'>
-				<h1>Social medias</h1>
-				<a target='_blank' href='https://www.instagram.com/stemguy.club/' className='social' >
-					<FiInstagram size={25} />
-					<span>@stemguy.club</span>
-				</a>
-				<a target='_blank' href='https://twitter.com/stemguyclub' className='social' >
-					<FiTwitter size={25} />
-					<span>@stemguyclub</span>
-				</a>
+			<div className='links'>
+				<div className='group'>
+					<h1>Site links</h1>
+					<ul>
+						<Link href='/' >
+							<a className='link' >Home</a>
+						</Link>
+						<Link href='/about' >
+							<a className='link' >About</a>
+						</Link>
+						<Link href='/about/privacy-policy' >
+							<a className='link' >Privacy policy</a>
+						</Link>
+					</ul>
+				</div>
+				<div className='group'>
+					<h1>Social medias</h1>
+					<ul>
+						<a target='_blank' href='https://www.instagram.com/stemguy.club/' className='social' >
+							<FiInstagram size={25} />
+							<span>@stemguy.club</span>
+						</a>
+						<a target='_blank' href='https://twitter.com/stemguyclub' className='social' >
+							<FiTwitter size={25} />
+							<span>@stemguyclub</span>
+						</a>
+					</ul>
+				</div>
 			</div>
 		</Container>
 	)
--- src/styles/components/Footer.ts
+++ src/styles/components/Footer.ts
@@ -6,7 +6,8 @@ const Container = styled.footer`
 	padding-bottom: 3rem;
 
 	display: flex;
-	justify-content: space-around;
+	align-items: center;
+	justify-content: space-between;
 	
 	*
 	{
@@ -14,64 +15,109 @@ const Container = styled.footer`
 		font-family: Ubuntu
 	}
 
-	.group
+	.logos
 	{
 		display: flex;
 		flex-direction: column;
-		gap: 1.25rem;
+		align-items: center;
+		gap: 1rem;
 
-		h1
+		width: 40rem;
+
+		.icon
 		{
-			font-weight: 700;
-			font-size: 2.5rem;
-
-			border-left: ${p => p.theme.colors.background} 5px solid;
-			padding-left: 1rem;
+			width: 15rem;
 		}
 
-		a
+		.name
 		{
-			margin-left: 2rem;
-			text-decoration: none;
+			width: 30rem;
 		}
+	}
 
-		.link
-		{
-			font-size: 1.75rem;
-			width: fit-content;
+	.links
+	{
+		width: 50%;
 
-			::after
-			{
-				content: '';
-				width: 0px;
-				height: 2px;
-				display: block;
-				background: ${p => p.theme.colors.background};
-				transition: 0.25s;
-			}
-			:hover::after
-			{
-				width: 100%;
-			}
-		}
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
 
-		.social
+		.group
 		{
 			display: flex;
-			align-items: center;
-			gap: 0.5rem;
+			flex-direction: column;
+			gap: 1rem;
 
-			transition: 0.25s;
+			width: 100%;
 
-			:hover
+			h1
 			{
-				text-shadow: 0 0 5px ${p => p.theme.colors.background};
-				transform: scale(1.05);
+				font-weight: 700;
+				font-size: 2.5rem;
+
+				border-left: ${p => p.theme.colors.background} 5px solid;
+				padding-left: 1rem;
 			}
 
-			span
+			ul
 			{
-				font-size: 1.5rem;
+				margin-left: 2rem;
+
+				display: grid;
+				grid-auto-rows: 3rem;
+				grid-template-columns: repeat(auto-fill, minmax(13.5rem, 1fr));
+				grid-gap: 1rem;
+
+				align-items: center;
+				/* justify-items: center; */
+			
+				a
+				{
+					text-decoration: none;
+				}
+
+				.link
+				{
+					font-size: 1.75rem;
+					width: fit-content;
+
+					::after
+					{
+						content: '';
+						width: 0px;
+						height: 2px;
+						display: block;
+
+						background: ${p => p.theme.colors.background};
+						transition: 0.25s;
+					}
+
+					:hover::after
+					{
+						width: 100%;
+					}
+				}
+
+				.social
+				{
+					display: flex;
+					align-items: center;
+					gap: 0.5rem;
+
+					transition: 0.25s;
+
+					:hover
+					{
+						text-shadow: 0 0 5px ${p => p.theme.colors.background};
+						transform: scale(1.05);
+					}
+
+					span
+					{
+						font-size: 1.5rem;
+					}
+				}
 			}
 		}
 	}
